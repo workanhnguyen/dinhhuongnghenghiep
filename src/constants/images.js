e095552c9a7a2f6fe10de3ac4f942f9026a5638d
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-01-28 16:24:05 +0700

    init

---
+++ src/constants/images.js
@@ -0,0 +1,36 @@
+import one from '../assets/one.png';
+import two from '../assets/two.png';
+import three from '../assets/three.png';
+import four from '../assets/four.png';
+import five from '../assets/five.png';
+import six from '../assets/six.png';
+import favicon from '../assets/favicon.png';
+
+import aboutimg from '../assets/aboutimg.svg';
+import aside from '../assets/aside.svg';
+import aside2 from '../assets/aside2.svg';
+import aside3 from '../assets/aside3.svg';
+import aside4 from '../assets/aside4.svg';
+import hero from '../assets/hero.svg';
+import logo from '../assets/logo.svg';
+import serviceIcon1 from '../assets/service-icon1.svg';
+import serviceIcon2 from '../assets/service-icon2.svg';
+
+export default {
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    favicon,
+    aboutimg,
+    aside,
+    aside2,
+    aside3,
+    aside4,
+    hero,
+    logo,
+    serviceIcon1,
+    serviceIcon2,
+};
---
+++ src/container/index.jsx
@@ -0,0 +1,5 @@
+export { default as About } from './About/About'
+export { default as Footer } from './Footer/Footer'
+export { default as Header } from './Header/Header'
+export { default as Supply } from './Supply/Supply'
+export { default as University } from './University/University'
