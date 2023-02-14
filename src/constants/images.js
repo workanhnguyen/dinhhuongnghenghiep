c28c07bdae46af1136d6082a5800b2768d142622
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-02-14 15:13:56 +0700

    update

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,77 +1,27 @@
-import one from '../assets/one.png';
-import two from '../assets/two.png';
-import three from '../assets/three.png';
-import four from '../assets/four.png';
-import five from '../assets/five.png';
-import six from '../assets/six.png';
-import favicon from '../assets/favicon.png';
+
 import cover from '../assets/cover.png';
 import holland from '../assets/holland.png';
 import back from '../assets/back.jpg';
 import defaultUser from '../assets/default-user.png';
 import loginHead from '../assets/login-head.jpg';
 import aboutimg from '../assets/aboutimg.svg';
-import aside from '../assets/aside.svg';
-import aside2 from '../assets/aside2.svg';
-import aside3 from '../assets/aside3.svg';
-import aside4 from '../assets/aside4.svg';
+
 import hero from '../assets/hero.png';
 import logo from '../assets/logo.png';
 import serviceIcon1 from '../assets/service-icon1.jpg';
 import serviceIcon2 from '../assets/service-icon2.jpg';
 
-import logoOU from '../assets/logo-ou.png';
-import logoBK from '../assets/logo-bk.png';
-import logoUEH from '../assets/logo-ueh.png';
-import logoUEL from '../assets/logo-uel.png';
-import logoUIT from '../assets/logo-uit.png';
-import logoUSSH from '../assets/logo-ussh.png';
-import logoFTU from '../assets/logo-ftu.png';
-import logoUTE from '../assets/logo-ute.png';
-import logoIUH from '../assets/logo-iuh.png';
 
-import realistic from '../assets/realistic.jpg';
-import investigative from '../assets/investigative.jpg';
-import artistic from '../assets/artistic.png';
-import enterprising from '../assets/enterprising.png';
-import social from '../assets/social.jpg';
-import conventional from '../assets/conventional.jpg';
 
 export default {
-    one,
-    two,
-    three,
-    four,
-    five,
-    six,
-    favicon,
-    aboutimg,
     cover,
+    holland,
     back,
     defaultUser,
-    holland,
     loginHead,
-    aside,
-    aside2,
-    aside3,
-    aside4,
+    aboutimg,
     hero,
     logo,
     serviceIcon1,
     serviceIcon2,
-    logoBK,
-    logoOU,
-    logoUEH,
-    logoUEL,
-    logoUIT,
-    logoUSSH,
-    logoFTU,
-    logoIUH,
-    logoUTE,
-    realistic,
-    investigative,
-    artistic,
-    conventional,
-    enterprising,
-    social,
 };
